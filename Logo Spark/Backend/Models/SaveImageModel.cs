24b6d06788c96fabf1d803736a158ca4e7e95765
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2025-07-25 18:12:04 -0600

    Delete Logo Spark directory

--- Logo Spark/Backend/Models/SaveImageModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LogoSpark.Models
-{
-    public class SaveImageModel
-    {
-        public string ImagePath { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-    }
-}
